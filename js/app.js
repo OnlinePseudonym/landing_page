a7614adeebaa202179ef68df50af15b668c6e4be
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2018-03-08 22:07:12 -0700

    add some HTML content style layout

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/* const nav = document.querySelector('.header--nav');
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+    console.log(window.scrollY, topOfNav)
+    if(window.scrollY >= topOfNav) {
+        nav.classList.add('fixed-nav');
+    } else {
+        nav.classList.remove('fixed-nav');
+    }
+}
+
+window.addEventListener('scroll', fixNav); */
